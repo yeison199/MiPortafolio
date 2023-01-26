c8ca0ffc87677c227019602b35ee1ef61a8a16b5
Yeison <97485473+yeison199@users.noreply.github.com>
2023-01-26 03:07:41 +0000

    asd

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('.menu a').each(function(index, elemento) {
+        $(this).css({
+            'top' : '-200px'
+        });
+
+        $(this).animate({
+            top: '0'
+        }, 2000 + (index * 500));
+    });
+});
